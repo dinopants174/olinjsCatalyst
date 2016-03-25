bd9812a43d16a0a7fcf78ff406f5f0b305a7e4e4
Zoher Ghadyali <zoher.ghadyali@gmail.com>
2016-03-25 16:55:31 -0400

    Starting login, updated user model

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 
 var userSchema = mongoose.Schema({
   name: String,
-  fbId: String
+  fbId: String,
+  inspirations: [String],
+  uploads: [String]
 });
 
 module.exports = mongoose.model('users', userSchema);
---
+++ react_components/login.jsx
@@ -0,0 +1,9 @@
+var Login = React.createClass({
+    render: function(){
+        return (
+        	<a href='/auth/facebook'>Login with Facebook</a>
+        );
+    }
+});
+
+module.exports = Login;
