4608d31cacf84fa84bb975b97bcc4ccc4a6719ef
Zoher Ghadyali <zoher.ghadyali@gmail.com>
2016-03-30 17:34:01 -0400

    Added title field for each piece

--- models/pieceModel.js
+++ models/pieceModel.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var pieceSchema = mongoose.Schema({
   author: {type: String, required: true},
   src: {type: String, required: true},
-  date: {type: Date, required: true}
+  date: {type: Date, required: true},
+  title: {type: String, required: true}
 });
 
 module.exports = mongoose.model('pieces', pieceSchema);
