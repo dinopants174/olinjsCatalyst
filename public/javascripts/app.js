e23e71d1a7e0be4a742c0725e5e6f71a8ae5cfff
Casey Alvarado <alvarado.casey@gmail.com>
2016-03-30 17:30:11 -0400

    made feed with static online images

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -20956,7 +20956,9 @@ var fs = require('fs');
 
 var images = ['http://www.google.com/logos/2011/thanksgiving-2011-hp.jpg',
 'http://searchengineland.com/figz/wp-content/seloads/2014/11/Thanksgiving-2014-Google-logo.png', 
-'http://www.pawderosa.com/images/puppies.jpg']; 
+'http://www.pawderosa.com/images/puppies.jpg', 
+'https://i.vimeocdn.com/portrait/10277584_300x300.jpg', 
+'http://www.tiptopcanning.com/images/big-tomatoes.png']; 
 
 
 var Feed = React.createClass({displayName: "Feed", 
@@ -20971,25 +20973,21 @@ var Feed = React.createClass({displayName: "Feed",
 	}, 
 
 	render: function(){ 
-		// var images_divs = images.map(function(elem) {
-		// 	console.log("elem", elem)
-		// 	return <img src={elem}/> 
-		// });
-
-		var image_divs = Array.apply(null, {length : 3}).map(function(elem, i) {
+		var image_divs = Array.apply(null, {length : 5}).map(function(elem, i) {
 			console.log(i)
 			console.log(elem)
-			console.log(images[i])
+			console.log('image size', images[i])
 			return React.createElement("td", {key: 'td'+i}, React.createElement("img", {key: 'td'+i, src: images[i]}))
 		});
 
-		var rows = Array.apply(null, {length: 1}).map(function(elem, i) {
-			// console.log("slices", image_divs.slice(7*(i), 7*(i+1)))
-			return React.createElement("tr", {key: 'tr'+i}, image_divs.slice(1*(i), 1*(i+1)))
+		console.log("image_divs", image_divs)
+		var rows = Array.apply(null, {length: 2}).map(function(elem, i) {
+			// console.log("slices", image_divs.slice(1*(i), 1*(i+1)))
+			return React.createElement("tr", {key: 'tr'+i}, image_divs.slice(3*(i), 3*(i+1)))
 		})
 
 		return(
-			React.createElement("div", {className: "feed"}, 
+			React.createElement("div", {id: "feed"}, 
 				React.createElement("table", {id: "feedItems"}, 
 					React.createElement("tbody", null, 
 						rows
