77d7421fb7456e99b19821f0a0ca7813fa5af037
yunhsincynthiachen <yun-hsin.chen@students.olin.edu>
2016-04-04 19:58:24 -0400

    with the feed layout looking like pinterest

--- react_components/feed.jsx
+++ react_components/feed.jsx
@@ -4,6 +4,11 @@ var dir = require('node-dir');
 var path = require('path'); 
 var fs = require('fs');
 
+var Masonry = require('./masonry.jsx');
+
+var masonryOptions = {
+    transitionDuration: 0
+};
 // var images = ['http://www.google.com/logos/2011/thanksgiving-2011-hp.jpg',
 // 'http://searchengineland.com/figz/wp-content/seloads/2014/11/Thanksgiving-2014-Google-logo.png', 
 // 'http://www.pawderosa.com/images/puppies.jpg', 
@@ -15,7 +20,7 @@ var Feed = React.createClass({
 
 	getInitialState: function(){ 
 		return{
-			images: this.props.feedObjects
+			images: this.props.feedObjects,
 		};
 	}, 
 
@@ -25,26 +30,52 @@ var Feed = React.createClass({
 
 	render: function(){ 
 
-		var parent = this; 
-		var image_divs = this.state.images.map(function(elem, i) {
-			return <td key={'td'+i}><div key={'td'+i} dangerouslySetInnerHTML = {parent.rawMarkup(elem.src)}/></td>
-		});
+		// var parent = this; 
+		// var image_divs = this.state.images.map(function(elem, i) {
+		// 	return <td key={'td'+i}><div key={'td'+i} dangerouslySetInnerHTML = {parent.rawMarkup(elem.src)}/></td>
+		// });
 
-		var rows = Array.apply(null, {length: 2}).map(function(elem, i) {
-			return <tr key={'tr'+i}>{image_divs.slice(3*(i), 3*(i+1))}</tr>
-		})
+		// var rows = Array.apply(null, {length: 2}).map(function(elem, i) {
+		// 	return <tr key={'tr'+i}>{image_divs.slice(3*(i), 3*(i+1))}</tr>
+		// })
 
 		return(
-			<div id= "feed"> 
-				<table id = "feedItems"> 
-					<tbody> 
-						{rows}
-					</tbody>
-				</table>
+			<div id="feed"> 
+				<Gallery images={this.state.images}/>
 			</div>
 		); 
 	}
 
 }); 
+
+var Gallery = React.createClass({
+
+	rawMarkup: function(e){ 
+		return {__html: e}
+	},
+
+    render: function () {
+		var parent = this; 
+        var childElements = this.props.images.map(function(element, i){
+           return (
+           		<div className="image-div-class">
+	                <div key={'div'+i} dangerouslySetInnerHTML={parent.rawMarkup(element.src)}/>
+	            	<p>{element.title}</p>
+	            	<a href="" className="button add"> + Add Inspiration</a>
+	            </div>
+            );
+        });
+
+        return (
+            <Masonry
+                className={'my-gallery-class'}
+                elementType={'div'}
+                disableImagesLoaded={false}
+            >
+                {childElements}
+            </Masonry>
+        );
+    }
+});
 
 module.exports = Feed; 
